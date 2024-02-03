3b4eacd35c30e5219de20f7d9afc853806af9d57
yoana <yewonyoana@gmail.com>
2024-02-03 20:50:09 +0900

    Updated File

--- js/memo.js
+++ js/memo.js
@@ -2,6 +2,12 @@ const memoForm = document.getElementById("memo-form");
 const memoInput = memoForm.querySelector("input");
 const memoList = document.getElementById("memo-list");
 
+const memos = [];
+
+function saveMemos() {
+	localStorage.setItem("memos", JSON.stringify(memos));
+}
+
 function deleteMemo(event) {
 	const li = event.target.parentElement;
 	li.remove();
@@ -23,7 +29,9 @@ function handleMemoSubmit(event) {
 	event.preventDefault();
 	const newMemo = memoInput.value;
 	memoInput.value = "";
+	memos.push(newMemo);
 	paintMemo(newMemo);
+	saveMemos();
 }
 
 memoForm.addEventListener("submit", handleMemoSubmit);
--- js/switch.js
+++ js/switch.js
@@ -1,27 +1,40 @@
 const workBtn = document.querySelector("#work-btn");
 const playBtn = document.querySelector("#play-btn");
+const nameBtn = document.querySelector("#greeting");
 const memoBox = document.querySelector("#memo-box");
 const memeBox = document.querySelector("#meme-box");
 
 const SWITCH_CLASSNAME = "switch";
 
-function onWorkBtn() {
+function onWorkBtnClick() {
 	memeBox.classList.add(SWITCH_CLASSNAME);
 	memoBox.classList.remove(SWITCH_CLASSNAME);
+	workBtn.style.opacity = 0.1;
+	playBtn.style.opacity = 1;
 }
 
-function onPlayBtn() {
+function onPlayBtnClick() {
 	memoBox.classList.add(SWITCH_CLASSNAME);
 	memeBox.classList.remove(SWITCH_CLASSNAME);
+	playBtn.style.opacity = 0.1;
+	workBtn.style.opacity = 1;
 }
 
-workBtn.addEventListener("click", onWorkBtn);
-playBtn.addEventListener("click", onPlayBtn);
-
-if (onWorkBtn === null) {
+function onNameBtnClick() {
 	memoBox.classList.add(SWITCH_CLASSNAME);
-	memeBox.classList.remove(SWITCH_CLASSNAME);
-} else {
-	memoBox.classList.add(SWITCH_CLASSNAME);
-	memoBox.classList.remove(SWITCH_CLASSNAME);
+	memeBox.classList.add(SWITCH_CLASSNAME);
+	workBtn.style.opacity = 1;
+	playBtn.style.opacity = 1;
 }
+
+workBtn.addEventListener("click", onWorkBtnClick);
+playBtn.addEventListener("click", onPlayBtnClick);
+nameBtn.addEventListener("click", onNameBtnClick);
+
+// if (onWorkBtn === null) {
+// 	memoBox.classList.add(SWITCH_CLASSNAME);
+// 	memeBox.classList.remove(SWITCH_CLASSNAME);
+// } else {
+// 	memoBox.classList.add(SWITCH_CLASSNAME);
+// 	memoBox.classList.remove(SWITCH_CLASSNAME);
+// }
